4b99ed80feec132a74976e1c754f5fec02f83840
Renan Augusto dos Santos <devrsantos@gmail.com>
2019-10-21 11:04:50 -0300

    Criando página de teste de requições

--- src/app/routes/route_testes_api.js
+++ src/app/routes/route_testes_api.js
@@ -1,23 +1,29 @@
 module.exports = application => {
-    application.get('/get', (req, res) => {
+    application.get('/page_teste_api', (req, res) => {
+        res.render('page_teste_api');
+    });
+}
+
+module.exports = application => {
+    application.get('/page_teste_api/get', (req, res) => {
         application.app.controllers.controller_testes_api.controller_test_get(application, req, res);
     });
 }
 
 module.exports = application => {
-    application.post('/post', (req, res) => {
+    application.post('/page_teste_api/post', (req, res) => {
         application.app.controllers.controller_testes_api.controller_test_post(application, req, res);
     });
 }
 
 module.exports = application => {
-    application.put('/put', (req, res) => {
+    application.put('/page_teste_api/put', (req, res) => {
         application.app.controllers.controller_testes_api.controller_test_put(application, req, res);
     });
 }
 
 module.exports = application => {
-    application.delete('/delete', (req, res) => {
+    application.delete('/page_teste_api/delete', (req, res) => {
         application.app.controllers.controller_testes_api.controller_test_delete(application, req, res);
     });
 }
