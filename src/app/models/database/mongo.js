3740bcd1822b42223f386af6a109cabbbdcd8c9e
Renan Augusto dos Santos <devrsantos@gmail.com>
2019-10-30 16:56:19 -0300

    Validando função no MongoDB

--- src/app/models/database/mongo.js
+++ src/app/models/database/mongo.js
@@ -3,74 +3,82 @@ const { MongoClient } = require('mongodb');
 const parametros = require('./parametros');
 
 const fn_mongo = (fn, query = {}, dados = {}) => {
-    MongoClient.connect(parametros.url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
-        
-        if (fn == 'findOne') {
-            const db = client.db(parametros.dbName);
-            const documents = db.parametros.collectionNames._.findOne(dados);
-            client.close();
-        }
 
-        if (fn == 'insertOne') {
-            const db = client.db(parametros.dbName);
-            try {
-                db.parametros.collectionNames._.insertOne(dados);
-            } catch (error) {
-                print(error);
-            };
-            client.close();
-        }
+    if (['findOne', 'insertOne', 'insertMany', 'deleteOne', 'deleteMany', 'updateOne', 'updateMany'].includes(fn)) {
 
-        if (fn == 'insertMany') {
-            const db = client.db(parametros.dbName);
-            try {
-                db.parametros.collectionNames._.insertMany(dados);
-            } catch (error) {
-                print(error);
-            };
-            client.close();
-        }
+        MongoClient.connect(parametros.url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
 
-        if (fn == 'deleteOne') {
-            const db = client.db(parametros.dbName);
-            try {
-                db.parametros.collectionNames._.deleteOne(query, dados);
-            } catch (error) {
-                print(error);
-            };
-            client.close();
-        }
+            if (fn == 'findOne') {
+                const db = client.db(parametros.dbName);
+                const documents = db.parametros.collectionNames._.findOne(dados);
+                documents.then(response => response.json().then(result => console.log(result)));
+                client.close();
+            }
 
-        if (fn == 'deleteMany') {
-            const db = client.db(parametros.dbName);
-            try {
-                db.parametros.collectionNames._.deleteMany(query, dados);
-            } catch (error) {
-                print(error);
-            };
-            client.close();
-        }
+            if (fn == 'insertOne') {
+                const db = client.db(parametros.dbName);
+                try {
+                    db.parametros.collectionNames._.insertOne(dados);
+                } catch (error) {
+                    print(error);
+                };
+                client.close();
+            }
 
-        if (fn == 'updateOne') {
-            const db = client.db(parametros.dbName);
-            try {
-                db.parametros.collectionNames._.updateOne(query, dados);
-            } catch (error) {
-                print(error);
-            };
-            client.close();
-        }
+            if (fn == 'insertMany') {
+                const db = client.db(parametros.dbName);
+                try {
+                    db.parametros.collectionNames._.insertMany(dados);
+                } catch (error) {
+                    print(error);
+                };
+                client.close();
+            }
 
-        if (fn == 'updateMany') {
-            const db = client.db(parametros.dbName);
-            try {
-                db.parametros.collectionNames._.updateOne(query, dados);
-            } catch (error) {
-                print(error);
-            };
-            client.close();
-        }
-    });
+            if (fn == 'deleteOne') {
+                const db = client.db(parametros.dbName);
+                try {
+                    db.parametros.collectionNames._.deleteOne(query, dados);
+                } catch (error) {
+                    print(error);
+                };
+                client.close();
+            }
+
+            if (fn == 'deleteMany') {
+                const db = client.db(parametros.dbName);
+                try {
+                    db.parametros.collectionNames._.deleteMany(query, dados);
+                } catch (error) {
+                    print(error);
+                };
+                client.close();
+            }
+
+            if (fn == 'updateOne') {
+                const db = client.db(parametros.dbName);
+                try {
+                    db.parametros.collectionNames._.updateOne(query, dados);
+                } catch (error) {
+                    print(error);
+                };
+                client.close();
+            }
+
+            if (fn == 'updateMany') {
+                const db = client.db(parametros.dbName);
+                try {
+                    db.parametros.collectionNames._.updateOne(query, dados);
+                } catch (error) {
+                    print(error);
+                };
+                client.close();
+            }
+
+        });
+    } else {
+        console.log("Função invalida");
+    }
 };
 
 module.exports = fn_mongo;
